d749e838323ab5b3c6710a00d797c31307b33a0d
Jonathan Navarro <olivergt123@gmail.com>
2022-03-08 02:21:20 -0300

    update - formato de muestra de % y grs para los ingredientes y analiticos en lar tarjetas de información de los alimentos

--- AlimentoMascotas/Frontend/AlimentoMascotas/src/app/shared/components/products/products.component.ts
+++ AlimentoMascotas/Frontend/AlimentoMascotas/src/app/shared/components/products/products.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { API_INTERNAL_ROUTES, ERROR_CONST, IMAGES_ROUTES } from '@data/constants';
-import { IApiAlimento, IApiResponse } from '@data/interfaces';
+import { IApiAlimento, IApiAnalitico, IApiIngrediente, IApiResponse } from '@data/interfaces';
 import { AlimentoService } from '@data/services';
 
 @Component({
@@ -18,16 +18,39 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     this.alimentoService.getAlimentos(API_INTERNAL_ROUTES.ALIMENTO.LISTAR, ERROR_CONST.ALIMENTO.DEFAULT_ERROR)
-      .subscribe((r: IApiResponse) => {
-        if (!r.error) {
-          var alimentos: IApiAlimento[] = r.data;
-          alimentos.forEach((alimento: IApiAlimento) => {
-            this.products.push(alimento);
-          })
-        } else {
-          console.log(r.message);
-        }
-      })
+    .subscribe((r: IApiResponse) => {
+      if (!r.error) {
+        var alimentos: IApiAlimento[] = r.data;
+        alimentos.forEach((alimento: IApiAlimento) => {
+          this.products.push(alimento);
+        })
+      } else {
+        console.log(r.message);
+      }
+    });
+  }
+  // CONVERTIR EN LISTA LA LISTA DE FORMATOS DE TAMAÑOS DEL ALIMENTO
+  // ORDENAR LOS INGREDIENTES, ANALITICOS Y ADITIVOS POR ORDEN DE CANTIDAD DESCENDEBNTE
+  // AÑADIR UN BOTÓN DE QUITAR TODOS LOS FILTROS
+  getQuantityIngrediente(ingrediente: IApiIngrediente): string {
+    if (ingrediente.quantityPer != 0) {
+      return ingrediente.name + " " + ingrediente.quantityPer + "%";
+    }
+    else {
+      return ingrediente.name;
+    }
+  }
+
+  getQuantityAnalitico(analitico: IApiAnalitico): string {
+    if (analitico.quantityGra != 0) {
+      return analitico.name + " " + analitico.quantityGra + "grs";
+    }
+    else if (analitico.quantityPer != 0) {
+      return analitico.name + " " + analitico.quantityPer + "%";
+    }
+    else {
+      return analitico.name;
+    }
   }
 
   imageRouteGenerator(key: string, path: string): string {
